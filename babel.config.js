a16a6080a2dee15cbce36d1107c4e7490744dfb6
polluterofminds <justin.edward.hunter@gmail.com>
2023-12-28 17:48:59 -0600

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function (api) {
+  api.cache(true);
+  const plugins = [];
+
+  plugins.push('nativewind/babel');
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins,
+  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,ts,tsx}', './src/**/*.{js,ts,tsx}'],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
