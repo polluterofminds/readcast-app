67874417ebb813062a78d28f2e9d63aecb39de7e
polluterofminds <justin.edward.hunter@gmail.com>
2024-01-24 08:25:03 -0600

    added an update notification and button

--- App.tsx
+++ App.tsx
@@ -6,10 +6,12 @@ import RNRestart from 'react-native-restart';
 import useError from 'hooks/useError';
 import 'text-encoding-polyfill';
 import { UserProvider } from 'contexts/UserContext';
-import FlashMessage from "react-native-flash-message";
+import FlashMessage, { hideMessage, showMessage } from "react-native-flash-message";
 import { LibraryProvider } from 'contexts/LibraryContext';
 import { MenuProvider } from 'react-native-popup-menu';
 // import 'expo-dev-client';
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
 
 export default function App() {
   const { submitError } = useError();
@@ -22,6 +24,31 @@ export default function App() {
     'Metropolis-Medium': require('./assets/fonts/Metropolis-Medium.otf'),
     'Metropolis-Regular': require('./assets/fonts/Metropolis-Regular.otf'),
   });
+
+  useEffect(() => {
+    checkForUpdates();
+  }, []);
+
+  const checkForUpdates = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        showMessage({
+          type: "info", 
+          message: "A new update is available, click to install",
+          onPress: async () => {
+            await Updates.fetchUpdateAsync();
+            await Updates.reloadAsync();
+            hideMessage();
+          }
+        })        
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      console.log("Error fetching updates");
+    }
+  }
 
   if (!fontsLoaded && !fontError) {
     return null;
