3db57d3a90f206126c53d9620e682236405f5af7
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-10 09:13:36 -0600

    updated config

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants'
 
-export const REACT_APP_API_URL = "https://readcast-server-production.up.railway.app"
+export const REACT_APP_API_URL = "https://api.readcast.xyz"
 // export const REACT_APP_API_URL = `http://${Constants?.expoConfig?.hostUri?.split(`:`)?.shift()?.concat(`:3000`)}`
 export const REACT_APP_NEYNAR_API_KEY = "FE1D7D55-0DD7-4BF6-8C74-48AC7C6CBA55"
 export const REACT_APP_NEYNAR_CLIENT_ID = "50fb47d0-06a0-42d1-ad86-8858d6d79f73"
