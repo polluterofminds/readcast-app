0014a6d1d350e068f5931dfd9f0e0a8ccace92ef
polluterofminds <justin.edward.hunter@gmail.com>
2024-04-23 12:28:02 -0500

    updated api URL

--- readcast/Managers/ConfigManager.swift
+++ readcast/Managers/ConfigManager.swift
@@ -9,6 +9,6 @@ import Foundation
 
 class ConfigManager {
     static let shared = ConfigManager()
-    var apiUrl: String = "https://c941-66-68-201-142.ngrok-free.app"
+    var apiUrl: String = "https://api.readcast.xyz"
     var primaryFont: String = "Shapiro 95 Super Extd"
 }
